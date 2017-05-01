23203179606357675b6c593db87b6dfcb3f9e07a
galihairlangga <galiherlangga7@gmail.com>
2017-05-02 03:25:53 +0700

    first commit

--- modul14/barang_cari.php
+++
@@ -1,9 +0,0 @@
-<?php include_once("template_atas.php"); ?>
-
-<form action="barang_tampil.php" method="post">
-	<h2>.:: CARI BARANG ::.</h2>
-	Nama Barang <input type="text" name="nama_barang" />
-	<input type="submit" value="Cari" />
-</form>
-
-<?php include_once("template_bawah.php"); ?>
--- modul14/index.php
+++
@@ -1,6 +0,0 @@
-<?php include_once("template_atas.php"); ?>
-
-<h1>SELAMAT DATANG DI TOKO KAMI</h2>
-<h1>" Nama Anda "</h2>
-
-<?php include_once("template_bawah.php"); ?>
